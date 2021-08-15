e5d1e5b901762c0cd56b5ac86cdae543297b9d34
Bima Indra <bimaindramulya@gmail.com>
2021-08-15 21:39:39 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+export class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>To be update...</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
